49ef8663760dc2dacb3da222df9ef2ac66e2f5f7
Progi1984 <progi1984@gmail.com>
2019-06-09 09:05:50 +0200

    WBox : Install / Conf Apache & PHP

--- scripts/03-install-tools.ps1
+++ scripts/03-install-tools.ps1
@@ -1,4 +1,40 @@
+# Firefox
 # https://chocolatey.org/packages/Firefox
 choco install -y firefox
+
+# LibreOffice
 # https://chocolatey.org/packages/libreoffice-fresh
 choco install -y libreoffice-fresh
+
+# Apache
+# https://brian.teeman.net/joomla/885-install-amp-on-windows-with-chocolatey
+# Note : WebPath : C:\Users\vagrant\AppData\Roaming\Apache24\htdocs
+choco install -y apache-httpd
+
+# Apache Conf
+copy "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf" "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd_backup.conf"
+(gc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf").replace('Listen 0.0.0.0:80','Listen 0.0.0.0:8080') | sc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf"
+(gc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf").replace('Listen [::0]:80','Listen [::0]:8080') | sc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf"
+(gc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf").replace('ServerName localhost:80','ServerName localhost:8080') | sc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf"
+# Apache Conf : IfModule dir_module
+(gc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf").replace('DirectoryIndex index.html','DirectoryIndex index.php index.html') | sc "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf"
+# Apache Conf : Support PHP
+Add-Content "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf" "AddHandler application/x-httpd-php .php"
+Add-Content "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf" "AddType application/x-httpd-php .php .html"
+Add-Content "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf" 'LoadModule php7_module "C:/tools/php73/php7apache2_4.dll"'
+Add-Content "C:\Users\vagrant\AppData\Roaming\Apache24\conf\httpd.conf" 'PHPIniDir "C:/tools/php73"'
+
+# PHP
+choco install -y php --params '"/ThreadSafe"'
+# PHP Conf
+copy "C:\tools\php73\php.ini" "C:\tools\php73\backup.php.ini"
+(gc "C:\tools\php73\php.ini").replace(';date.timezone =','date.timezone ="UTC"') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace('memory_limit = 128M','memory_limit = 512M') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace('upload_max_filesize = 2M','upload_max_filesize = 12M') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_curl.dll','extension=php_curl.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_gd2.dll','extension=php_gd2.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_fileinfo.dll','extension=php_fileinfo.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_mbstring.dll','extension=php_mbstring.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_mysqli.dll','extension=php_mysqli.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_pdo_mysql.dll','extension=php_pdo_mysql.dll') | sc "C:\tools\php73\php.ini"
+(gc "C:\tools\php73\php.ini").replace(';extension=php_openssl.dll','extension=php_openssl.dll') | sc "C:\tools\php73\php.ini"
