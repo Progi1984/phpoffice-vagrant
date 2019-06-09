67812171cf40e048a0672171385d2f301933f6c8
Progi1984 <progi1984@gmail.com>
2019-06-09 22:57:12 +0200

    WBox : Git Clone & Composer fixes

--- scripts/04-git-clone.ps1
+++ scripts/04-git-clone.ps1
@@ -1,8 +1,7 @@
 ### GIT Clone
-cmd /C "git clone --branch issue382 https://github.com/Progi1984/PHPPresentation.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPPresentation"
-# --branch develop PHPOffice
+cmd /C "git clone --branch develop https://github.com/PHPOffice/PHPPresentation.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPPresentation"
 cmd /C "git clone --branch develop https://github.com/PHPOffice/PHPProject.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPProject"
-cmd /C "git clone --master develop https://github.com/PHPOffice/PhpSpreadsheet.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PhpSpreadsheet"
+cmd /C "git clone --branch master https://github.com/PHPOffice/PhpSpreadsheet.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PhpSpreadsheet"
 cmd /C "git clone --branch develop https://github.com/PHPOffice/PHPVisio.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPVisio"
 cmd /C "git clone --branch develop https://github.com/PHPOffice/PHPWord.git C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPWord"
 
@@ -12,7 +11,7 @@ composer install
 cd C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPProject\
 composer install
 cd C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PhpSpreadsheet\
-composer install
+composer install --ignore-platform-reqs
 cd C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPVisio\
 composer install
 cd C:\Users\vagrant\AppData\Roaming\Apache24\htdocs\PHPWord\
